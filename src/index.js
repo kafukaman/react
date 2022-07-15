a0ffadf36a3a44c1ac175d730b613799f56d5538
kafukaman <68581664+kafukaman@users.noreply.github.com>
2022-07-15 14:27:35 +0900

    section4完了

--- src/index.js
+++ src/index.js
@@ -163,7 +163,7 @@
  * mapやfilterを作った配列処理
  */
 //map
-const nameArr = ["田中", "山田", "佐藤"];
+// const nameArr = ["田中", "山田", "佐藤"];
 // for (let i = 0; i < nameArr.length; i++) {
 //   console.log(nameArr[i]);
 // }
@@ -191,11 +191,55 @@ const nameArr = ["田中", "山田", "佐藤"];
 // });
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "佐藤") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "佐藤") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件 ? 条件がtrueの時 : 条件がfalseの時
+
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());　//1,300
+//toLocaleStringは文字列は使えない、数値のみ
+
+// const num = "1300";
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲以内です";
+// };
+// console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味を知ろう　&&（かつ） ||（または）
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになる");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになる");
+// }
+
+// || は左側がfalseなら右側を返す！！
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+//　&& は左側がtrueなら右側を返す！！
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
